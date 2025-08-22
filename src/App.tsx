4ae8180bffb10ebfbc9f74479888a829885c1b79
moidsufiyan <mmoidsufiyan4151@gmail.com>
2025-08-22 22:46:45 +0530

    backend and db removal

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,15 @@
 
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
 import Index from './pages';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Shop from './pages/Shop';
-import ProductDetail from './pages/ProductDetail';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
-import Login from './pages/Login';
-import Profile from './pages/Profile';
-import Wishlist from './pages/Wishlist';
-import UserAccount from './pages/UserAccount';
-import UserDashboard from './pages/UserDashboard';
-import OrderTracking from './pages/OrderTracking';
-import Support from './pages/Support';
-import Customize from './pages/Customize';
-import AdminDashboard from './pages/AdminDashboard';
-import NotFound from './pages/NotFound';
-import ScrollToTop from './components/ScrollToTop';
-import BundleDetail from "./pages/BundleDetail";
-import Bundles from "./pages/Bundles";
-import FAQ from "./pages/FAQ";
-import SizeGuidePage from './pages/SizeGuidePage';
-import Blog from './pages/Blog';
-import BlogPost from './pages/BlogPost';
-import AdminPanel from './pages/AdminPanel';
-import MockCheckout from './pages/MockCheckout';
-import CheckoutSuccess from './pages/CheckoutSuccess';
 
 function App() {
   return (
     <BrowserRouter>
-      <ScrollToTop />
       <div className="app">
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<MockCheckout />} />
-          <Route path="/checkout-success" element={<CheckoutSuccess />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/wishlist" element={<Wishlist />} />
-          <Route path="/user-account" element={<UserAccount />} />
-          <Route path="/user-dashboard" element={<UserDashboard />} />
-          <Route path="/order-tracking" element={<OrderTracking />} />
-          <Route path="/support" element={<Support />} />
-          <Route path="/customize" element={<Customize />} />
-          <Route path="/admin-dashboard" element={<AdminDashboard />} />
-          <Route path="/admin" element={<AdminPanel />} />
-          <Route path="/faq" element={<FAQ />} />
-          
-          {/* Bundle Routes */}
-          <Route path="/bundles" element={<Bundles />} />
-          <Route path="/bundle/:id" element={<BundleDetail />} />
-          
-          {/* Blog Routes */}
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:slug" element={<BlogPost />} />
-          
-          {/* Size Guide */}
-          <Route path="/size-guide" element={<SizeGuidePage />} />
-          
-          <Route path="*" element={<NotFound />} />
         </Routes>
-        <Toaster position="bottom-right" />
       </div>
     </BrowserRouter>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,41 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WishlistProvider } from './contexts/WishlistContext';
-import { CartProvider } from './contexts/CartContext';
-import { AuthProvider } from './contexts/AuthContext';
-import { ProductProvider } from './contexts/ProductContext';
-import { Toaster } from '@/components/ui/toaster';
-import App from './App.tsx';
-import './index.css';
-
-// Create a client
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: 1,
-    },
-  },
-});
-
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 if (root) {
   createRoot(root).render(
     <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <ProductProvider>
-            <CartProvider>
-              <WishlistProvider>
-                <App />
-                <Toaster />
-              </WishlistProvider>
-            </CartProvider>
-          </ProductProvider>
-        </AuthProvider>
-      </QueryClientProvider>
+      <App />
     </React.StrictMode>
   );
 }
